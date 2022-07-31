356a53940defeff93112acee3d3821f22160a7b0
Pedro Calado Moura <pcaladomoura@gmail.com>
2022-07-31 20:18:48 -0300

    conecct to sqlproject

---
+++ Java/frameworks/SpringBoot/connectionOnMysql/src/main/java/API/connectionOnMysql/ConnectionOnMysqlApplication.java
@@ -0,0 +1,13 @@
+package API.connectionOnMysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectionOnMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectionOnMysqlApplication.class, args);
+	}
+
+}
