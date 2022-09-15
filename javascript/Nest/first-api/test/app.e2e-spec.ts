9de48101c9d5307dfefbe4aceb8024983a64fa3a
Pedro Calado Moura <pcaladomoura@gmail.com>
2022-09-15 17:22:56 -0300

    add test and testdb

--- javascript/Nest/first-api/test/app.e2e-spec.ts
+++ javascript/Nest/first-api/test/app.e2e-spec.ts
@@ -1,11 +1,14 @@
 import { Test } from '@nestjs/testing'
-import { AppModule } from 'src/app.module'
-
+import { AppModule } from '../src/app.module'
+import { INestApplication, ValidationPipe } from '@nestjs/common'
 describe('App e2e', () => {
+  let app: INestApplication
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile()
+    app = moduleRef.createNestApplication()
+    app.useGlobalPipes(new ValidationPipe())
   })
-  it.todo('should pass')
+  it.todo('should pass') 
 })
