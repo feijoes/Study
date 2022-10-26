ca43a6f660941c52a4abc56fe3e604fa9c718736
Pedro Calado Moura <pcaladomoura@gmail.com>
2022-10-26 12:33:46 -0300

    put images and functions

--- javascript/React/React_native/firstApp/App.js
+++ javascript/React/React_native/firstApp/App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text,SafeAreaView,Image,TouchableOpacity   } from 'react-native';
 
 export default function App() {
+  const handlePress = ()=> console.log("Test")
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress}>Buratti is gay</Text>
       <StatusBar style="auto" />
-    </View>
+      <TouchableOpacity  onPress={handlePress}>
+        <Image onPress={handlePress} style={styles.stretch} source={require("./assets/poggersImage.png")} />
+      </TouchableOpacity >
+    </SafeAreaView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  stretch: {
+    width: 200,
+    height: 200,
+    resizeMode: 'stretch',
+    
   },
 });
