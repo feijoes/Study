5d2b40de218635d34a4bcd8d1472409fb642e04c
Pedro Calado Moura <pcaladomoura@gmail.com>
2022-11-15 13:31:59 -0300

    add connection to mongodb and jwt authentication

--- javascript/React/React_native/PostApp/Server/app.js
+++ javascript/React/React_native/PostApp/Server/app.js
@@ -1,7 +1,35 @@
-const express = require("express");
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const passport = require('passport')
+
+require('dotenv').config();
+require('./config/database');
+require('./models/user');
+require('./config/passport')(passport);
+
+
 const app = express();
+app.use(passport.initialize());
 
 
+app.use(express.json());
 
+app.use((_, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
-app.listen(8080);
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(require('./routes'));
+app.get("/",(req, res)=>{
+    return "this is the backend test"
+})
+
+app.listen(5000);
