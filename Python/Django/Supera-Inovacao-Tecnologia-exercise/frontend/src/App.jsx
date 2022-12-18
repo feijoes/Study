359f9e0687352800b87ede6618bea59196f8f8c1
Pedro Calado Moura <pcaladomoura@gmail.com>
2022-12-18 14:28:50 -0300

    add initial route for front

---
+++ Python/Django/Supera-Inovacao-Tecnologia-exercise/frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Home from './routes/Home/Home'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+function App() {
+  
+
+  return (
+    <div className='app'>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+   </div>
+  )
+}
+
+export default App
