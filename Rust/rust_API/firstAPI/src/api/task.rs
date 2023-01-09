85bb439ec193d9727f1ba31499c3c010af2de98b
Pedro Calado Moura <pcaladomoura@gmail.com>
2023-01-09 14:09:24 -0300

    update repo and add models

--- Rust/rust_API/firstAPI/src/api/task.rs
+++ Rust/rust_API/firstAPI/src/api/task.rs
@@ -8,7 +8,7 @@ use actix_web::{
     web::Json,
     web::Data,
     HttpResponse,
-    http::{header::ContentType, StatusCode}
+    http::{header::ContentType, StatusCode}, body
 };
 use serde::{Serialize, Deserialize};
 use strum_macros::{Display};
@@ -17,7 +17,28 @@ use strum_macros::{Display};
 pub struct TaskIdentifier{
     task_id: String,
 }
+#[derive(Deserialize,Serialize)]
+pub struct TaskCompletionRequest{
+    result_file: String,
+}
+
+#[derive(Deserialize)]
+pub struct SubmitTaskRequest{
+    user_id: String,
+    task_typep: String,
+    source_file: String
+}
+
+pub enum TaskError{
+    TaskNotFound,
+    TaskUpdateFailure,
+    TaskCreationFailure,
+    BadTaskRequest
+}
+
+
+
 #[get("/task/{task_id}")]
-pub async fn get_task(task_identifier: Path<TaskIdentifier>) -> Json<String>{
+pub async fn get_task(task_identifier: Path<TaskIdentifier>, body: Json<Str>) -> Json<String>{
     return Json(task_identifier.into_inner().task_id);
 }
--- Rust/rust_API/firstAPI/src/main.rs
+++ Rust/rust_API/firstAPI/src/main.rs
@@ -1,19 +1,34 @@
 mod api;
+mod repo;
+mod model;
 use api::task::{get_task};
 use actix_web::{HttpServer,App,web::Data,middleware::Logger};
 use std::io::Result;
 use std::env::set_var;
-
+use repo::ddb::DDBRepository;
 #[actix_web::main]
 async fn main() -> Result<()> {
     set_var("RUST_LOG", "debug");
     set_var("RUST_BACKTRACE", "1");
     env_logger::init();
-
+    
+    let config = aws_config::load_from_env().await;
+    
     HttpServer::new(move || {
-  
-        App::new().service(get_task)
-    }).bind(("127.0.0.1",80))?.run().await
+        let ddb_repo: DDBRepository = DDBRepository::init(
+            "task".to_string(),
+            config.clone()
+        );
+        let ddb_data: Data<DDBRepository> = Data::new(ddb_repo);
+    
+        App::new()
+        .wrap(Logger::default())
+        .app_data(ddb_data)
+        .service(get_task)
+    })
+    .bind(("127.0.0.1",80))?
+    .run()
+    .await
 
 
 }
