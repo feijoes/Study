36b5e03603d090091b44519dc20e50df8e39028f
Pedro Calado Moura <pcaladomoura@gmail.com>
2023-01-09 19:02:56 -0300

    update and add gitignore

--- Rust/rust_API/firstAPI/src/api/task.rs
+++ Rust/rust_API/firstAPI/src/api/task.rs
@@ -8,10 +8,13 @@ use actix_web::{
     web::Json,
     web::Data,
     HttpResponse,
-    http::{header::ContentType, StatusCode}, body
+    http::{header::ContentType, StatusCode}, body, cookie::Display
 };
 use serde::{Serialize, Deserialize};
 use strum_macros::{Display};
+use crate::model::task::Task;
+use crate::model::task::TaskState;
+
 
 #[derive(Deserialize,Serialize)]
 pub struct TaskIdentifier{
@@ -29,6 +32,7 @@ pub struct SubmitTaskRequest{
     source_file: String
 }
 
+#[derive(Debug,Display)]
 pub enum TaskError{
     TaskNotFound,
     TaskUpdateFailure,
@@ -36,9 +40,25 @@ pub enum TaskError{
     BadTaskRequest
 }
 
+impl ResponseError for TaskError {
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::build(self.status_code())
+            .insert_header(ContentType::json())
+            .body(self.to_string())
+    }
+    
 
+    fn status_code(&self) -> StatusCode {
+        match self {
+            TaskError::TaskNotFound => StatusCode::NOT_FOUND,
+            TaskError::TaskUpdateFailure => StatusCode::FAILED_DEPENDENCY,
+            TaskError::TaskCreationFailure => StatusCode::FAILED_DEPENDENCY,
+            TaskError::BadTaskRequest => StatusCode::BAD_REQUEST
+        }
+    }
+}
 
 #[get("/task/{task_id}")]
-pub async fn get_task(task_identifier: Path<TaskIdentifier>, body: Json<Str>) -> Json<String>{
+pub async fn get_task(task_identifier: Path<TaskIdentifier>) -> Result<Json<Task>,TaskState>{
     return Json(task_identifier.into_inner().task_id);
 }
