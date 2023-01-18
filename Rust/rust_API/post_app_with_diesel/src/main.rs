d984deec43cddf8ddf1b1e8cfc54556b85ea3a3f
Pedro Calado Moura <pcaladomoura@gmail.com>
2023-01-18 17:07:27 -0300

    improve main and add rocket.toml

--- Rust/rust_API/post_app_with_diesel/src/main.rs
+++ Rust/rust_API/post_app_with_diesel/src/main.rs
@@ -6,12 +6,31 @@ use rocket::serde::json::Json;
 fn index() -> &'static str {
     "Hello, world!"
 }
-#[get("/random")]
-fn get_random_blog_post() -> Json<Post>{
-    return Json(Post { id: 2, title: "test".to_string(), edited: false });
+#[get("/all")]
+fn get_all_posts() -> Json<Vec<Post>> {
+    return Json(vec![
+        Post {
+            id: 31,
+            title: "Some title".to_string(),
+            edited: false
+        },
+        Post {
+            id: 3441,
+            title: "Some 3q".to_string(),
+            edited: true
+        }
+      ]
+    )
+}
+#[post("/", data = "<blog_post>")]
+fn create_blog_post(blog_post: Json<Post>) -> Json<Post> {
+    blog_post
 }
 #[launch]
 fn rocket() -> _ {
     let rocket = rocket::build();
-    rocket.mount("/", routes![index])
-}
+    rocket
+        .mount("/", routes![index])
+        .mount("/", routes![get_all_posts])
+        .mount("/", routes![create_blog_post])
+    }
