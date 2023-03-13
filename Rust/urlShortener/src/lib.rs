711e4033b274174be0c027abfc44f24f9f8e950c
Pedro Calado Moura <pcaladomoura@gmail.com>
2023-03-13 17:54:02 -0300

    add home.html

--- Rust/urlShortener/src/lib.rs
+++ Rust/urlShortener/src/lib.rs
@@ -4,6 +4,8 @@ use web_sys::{Request, Response, Headers, ResponseInit};
 use url::Url;
 include!(concat!(env!("OUT_DIR"),"\\map.rs"));
 
+const HOME_PAGE: &str = include_str!("./home.html");
+
 #[wasm_bindgen]
 pub async fn handler(request: Request) -> Result<Response, JsValue> {
     console_error_panic_hook::set_once();
@@ -11,6 +13,10 @@ pub async fn handler(request: Request) -> Result<Response, JsValue> {
     let url: Url = request.url().parse().unwrap();
     let path = url.path();
     let mut init = ResponseInit::default();
+
+    if path == "/" {
+        return home(request).await;
+    }
 
     match MAPPINGS.binary_search_by_key(&path,|e| e.0) {
         Ok(index) => {
@@ -30,4 +36,12 @@ pub async fn handler(request: Request) -> Result<Response, JsValue> {
     }
     
     
+}
+pub async fn home(_request: Request) -> Result<Response, JsValue> {
+    let mut init = ResponseInit::default();
+    let headers = Headers::new()?;
+    headers.set("Content-Type", "text/html")?;
+    init.headers(&headers);
+
+    Response::new_with_opt_str_and_init(Some(HOME_PAGE), &init)
 }
