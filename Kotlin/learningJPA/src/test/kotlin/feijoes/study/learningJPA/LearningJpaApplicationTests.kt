ad9194b2c6583f0f2a5a33a787397f0e23470c00
Pedro Calado Moura <pcaladomoura@gmail.com>
2023-04-07 21:43:21 -0300

    add initial files for learning kotlin

---
+++ Kotlin/learningJPA/src/test/kotlin/feijoes/study/learningJPA/LearningJpaApplicationTests.kt
@@ -0,0 +1,13 @@
+package feijoes.study.learningJPA
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LearningJpaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
