e2fdc831fc432f49e5d8600a1b8e9503ea826dbd
feijoes <pcaladomoura@gmail.com>
2024-07-13 19:33:47 -0300

    improve run.sh

--- Pascal/run.sh
+++ Pascal/run.sh
@@ -10,7 +10,7 @@ FILE_NAME=$1
 fpc ${FILE_NAME}.pas
 
 if [ $? -eq 0 ]; then
-    echo "Compilation successful. Running the program..."
+    printf "Compilation successful. Running the program...\n\n"
     sudo ./${FILE_NAME}
     rm -f ${FILE_NAME}.o ${FILE_NAME}
     echo "Cleanup: Deleted ${FILE_NAME}.o and ${FILE_NAME}."
