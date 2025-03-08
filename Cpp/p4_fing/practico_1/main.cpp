7e484fba73636f9ba872fb3a9eada3ae2eb6b2ca
feijoes <pcaladomoura@gmail.com>
2025-03-07 23:40:11 -0300

    organize files and add p4

---
+++ Cpp/p4_fing/practico_1/main.cpp
@@ -0,0 +1,24 @@
+class Fecha {
+    public:
+        int dia;
+        int mes;
+        int anio;
+        Fecha(int dia, int mes, int anio): anio(anio), dia(dia), mes(mes) {}
+
+        int cant_dias_mes()
+        Fecha avanzar(int dias){
+            int dia = dia + dias;
+
+            return Fecha()
+        }
+}
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout<<"Hello World";
+
+    return 0;
+}
